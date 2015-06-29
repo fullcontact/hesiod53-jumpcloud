a0a1080f4aa7867fc9f52caaa9e33a3a93e008b1
Samuel Toriel <samuel@fullcontact.com>
2015-06-29 13:48:27 -0600

    Update README and setup.py info

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-readme = open('README.md').read()
+readme = open('README.rst').read()
 
 install_requires = [
     "pyyaml==3.11",
@@ -11,12 +11,23 @@ install_requires = [
 
 setup(
     name='hesiod53-jumpcloud',
-    version='0.1.0',
+    version='0.1.1',
     description='Create configuration file for hesiod53 using jumpcloud directory',
     long_description=readme,
     author='FullContact',
     author_email='ops+hesiod53@fullcontact.com',
     url='https://github.com/fullcontact/hesiod53-jumpcloud',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Systems',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='ssh jumpcloud',
     packages=find_packages(),
     include_package_data=True,
     install_requires=install_requires,
